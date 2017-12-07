0d4eb704221ae4905e92b853cafb06d42504cd25
son <son@DESKTOP-V3OLI0O>
2017-12-07 17:06:56 +0700

    bet bitmap to bytes and colors

--- XuLyAnh/Form1.Designer.cs
+++ XuLyAnh/Form1.Designer.cs
@@ -28,21 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.BtnBrowse1 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.BtnBrowse2 = new System.Windows.Forms.Button();
+            this.BtnSetPictureBox1 = new System.Windows.Forms.Button();
+            this.Lbl1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // BtnBrowse1
+            // 
+            this.BtnBrowse1.Location = new System.Drawing.Point(173, 447);
+            this.BtnBrowse1.Name = "BtnBrowse1";
+            this.BtnBrowse1.Size = new System.Drawing.Size(80, 40);
+            this.BtnBrowse1.TabIndex = 1;
+            this.BtnBrowse1.Text = "Browse";
+            this.BtnBrowse1.UseVisualStyleBackColor = true;
+            this.BtnBrowse1.Click += new System.EventHandler(this.BtnBrowse1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(566, 13);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // BtnBrowse2
+            // 
+            this.BtnBrowse2.Location = new System.Drawing.Point(726, 447);
+            this.BtnBrowse2.Name = "BtnBrowse2";
+            this.BtnBrowse2.Size = new System.Drawing.Size(80, 40);
+            this.BtnBrowse2.TabIndex = 3;
+            this.BtnBrowse2.Text = "Browse";
+            this.BtnBrowse2.UseVisualStyleBackColor = true;
+            this.BtnBrowse2.Click += new System.EventHandler(this.BtnBrowse2_Click);
+            // 
+            // BtnSetPictureBox1
+            // 
+            this.BtnSetPictureBox1.Location = new System.Drawing.Point(173, 584);
+            this.BtnSetPictureBox1.Name = "BtnSetPictureBox1";
+            this.BtnSetPictureBox1.Size = new System.Drawing.Size(80, 40);
+            this.BtnSetPictureBox1.TabIndex = 4;
+            this.BtnSetPictureBox1.Text = "Browse";
+            this.BtnSetPictureBox1.UseVisualStyleBackColor = true;
+            this.BtnSetPictureBox1.Click += new System.EventHandler(this.BtnSetPictureBox1_Click);
+            // 
+            // Lbl1
+            // 
+            this.Lbl1.AutoSize = true;
+            this.Lbl1.Location = new System.Drawing.Point(173, 680);
+            this.Lbl1.Name = "Lbl1";
+            this.Lbl1.Size = new System.Drawing.Size(51, 20);
+            this.Lbl1.TabIndex = 6;
+            this.Lbl1.Text = "label1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(278, 244);
+            this.ClientSize = new System.Drawing.Size(978, 744);
+            this.Controls.Add(this.Lbl1);
+            this.Controls.Add(this.BtnSetPictureBox1);
+            this.Controls.Add(this.BtnBrowse2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.BtnBrowse1);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button BtnBrowse1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button BtnBrowse2;
+        private System.Windows.Forms.Button BtnSetPictureBox1;
+        private System.Windows.Forms.Label Lbl1;
     }
 }
 
--- XuLyAnh/Form1.cs
+++ XuLyAnh/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,6 +14,23 @@ namespace XuLyAnh
 {
     public partial class Form1 : Form
     {
+
+        class MyPoint
+        {
+            int x;
+            int y;
+
+            public MyPoint(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+
+        private Bitmap bm1;
+        private Bitmap bm2;
+        private Dictionary<String, Color> dicColors;
+
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +38,163 @@ namespace XuLyAnh
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            MessageBox.Show("XuLyAnh");
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void BtnBrowse1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog()
+            {
+                Filter = "Image Files(*.BMP;*.JPG;*.GIF;*.PNG)|*.BMP;*.JPG;*.GIF;*.PNG",
+                FilterIndex = 1,
+                Multiselect = false
+            };
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if (ofd.FileName != null)
+                    {
+                        bm1 = new Bitmap(ofd.FileName);
+                        //mHandler.MyBitmap1 = (Bitmap)Bitmap.FromFile(ofd.FileName);
+                        pictureBox1.Image = bm1;
+                        byte[,,] arr1 = BitmapToBytes(bm1);
+                        BitmapToColors(bm1, arr1);
+                        var dic = BitmapToColors(bm1, arr1);
+                        dicColors = dic;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Could not open file! \n" + ex.Message);
+                }
+            }
+        }
+
+        private void BtnBrowse2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog()
+            {
+                Filter = "Image Files(*.BMP;*.JPG;*.GIF;*.PNG)|*.BMP;*.JPG;*.GIF;*.PNG",
+                FilterIndex = 1,
+                Multiselect = false
+            };
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if (ofd.FileName != null)
+                    {
+                        bm2 = new Bitmap(ofd.FileName);
+                        //mHandler.MyBitmap1 = (Bitmap)Bitmap.FromFile(ofd.FileName);
+                        pictureBox2.Image = bm2;
+                        byte[,,] arr2 = BitmapToBytes(bm2);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Could not open file! \n" + ex.Message);
+                }
+            }
+        }
+
+        private byte[,,] BitmapToBytes(Bitmap bitmap)
+        {
+            BitmapData bitmapData =
+                bitmap.LockBits(new Rectangle(new Point(), bitmap.Size), ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+            byte[] bitmapBytes;
+            var stride = bitmapData.Stride;
+            try
+            {
+                int byteCount = bitmapData.Stride * bitmap.Height;
+                bitmapBytes = new byte[byteCount];
+                Marshal.Copy(bitmapData.Scan0, bitmapBytes, 0, byteCount);
+            }
+            finally
+            {
+                bitmap.UnlockBits(bitmapData);
+            }
+            byte[,,] result = new byte[3, bitmap.Width, bitmap.Height];
+            for (int k = 0; k < 3; k++)
+            {
+                for (int i = 0; i < bitmap.Width; i++)
+                {
+                    for (int j = 0; j < bitmap.Height; j++)
+                    {
+                        result[k, i, j] = bitmapBytes[j * stride + i * 3 + k];
+                    }
+                }
+            }
+            return result;
+        }
+
+        private Dictionary<String, Color> BitmapToColors(Bitmap bitmap, byte[,,] arr)
+        {
+            var dic = new Dictionary<String, Color>();
+
+            for (int i = 0; i < bitmap.Width; i++)
+            {
+                for (int j = 0; j < bitmap.Height; j++)
+                {
+                    var r = int.Parse(arr[2, i, j].ToString());
+                    var g = int.Parse(arr[1, i, j].ToString());
+                    var b = int.Parse(arr[0, i, j].ToString());
+
+                    var color = Color.FromArgb(r, g, b);
+                    var point = i.ToString() + "," + j.ToString();
+
+                    dic.Add(point, color);
+                }
+
+            }
+            return dic;
+
+        }
+
+        private void ShowColors(Dictionary<String, Color> dic)
+        {
+            try
+            {
+                var image1 = bm1;
+
+                int x, y;
+
+                // Loop through the images pixels to reset color.
+                for (x = 0; x < image1.Width; x++)
+                {
+                    for (y = 0; y < image1.Height; y++)
+                    {
+                        Color pixelColor = image1.GetPixel(x, y);
+                        //Color newColor = Color.FromArgb(pixelColor.R, 0, 0);
+                        var point = x.ToString() + "," + y.ToString();
+                        //Color value = Color.Red;
+
+                        var color = dic[point];
+                        image1.SetPixel(x, y, color);
+
+                    }
+                }
+
+                // Set the PictureBox to display the image.
+                pictureBox2.Image = image1;
+
+                // Display the pixel format in Label1.
+                Lbl1.Text = "Pixel format: " + image1.PixelFormat.ToString();
+
+            }
+            catch (ArgumentException)
+            {
+                MessageBox.Show("There was an error." +
+                "Check the path to the image file.");
+            }
+        }
+
+        private void BtnSetPictureBox1_Click(object sender, EventArgs e)
+        {
+            ShowColors(dicColors);
         }
     }
 }
